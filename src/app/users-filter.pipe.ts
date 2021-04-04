3c29d26206caafc10475bef9e006fbda145180bd
Inbar Copitman <inbarcopitman@gmail.com>
2021-04-05 00:08:23 +0300

    - Added search filter functionality.

---
+++ src/app/users-filter.pipe.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'usersFilter'
+})
+export class UsersFilterPipe implements PipeTransform {
+
+  transform(users: any, input: any): any {
+
+    if (input) {
+      return users.filter(user => {
+        return user.Name.toLowerCase().includes(input.toLowerCase());
+      });
+    } else {
+      return users;
+    }
+  }
+
+
+}
